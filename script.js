2701381124f75fe1baa0e3b84ac5a69a9040e3a0
Vanesa Edo <vanesa.en@gmail.com>
2024-03-07 08:26:09 +0100

    Revisión 2º mapa.No se consigue la visualización.

--- script.js
+++ script.js
@@ -19,49 +19,27 @@ if ("geolocation" in navigator) {//coge datos de ubicación (coordenadas) del na
 
 //2 
 
-
-
 //Petición HTTP para obtener los terremotos disponibles en la API
 
-    async function getEarth() {
+    async function getEarthqu() {
         let response = await fetch('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson');
         let data = await response.json();
-        //console.log(data)
-        const map2 = await L.map('map2').setView([-122.7128296, 38.7514992],13);
-        L.tileLayer.provider('OpenTopoMap').addTo(map2);
-     
-        //const map2 = await L.map('map2').setView([data.features[0].geometry.coordinates[1], data.features[0].geometry.coordinates[2]],13);
-        return map2;
-        }
 
-async function pintar() {
+        //console.log(data)
+        const map2Id = 'map2'
+
+        const map2 = await L.map('map2').setView([data.features[0].geometry.coordinates[1], data.features[0].geometry.coordinates[2]],13);
+        //await L.map(map2Id).setView([40.4169473, 38.7514992],13);
+        L.tileLayer.provider('OpenTopoMap').addTo(map2);
+        return map2;
+        } 
+ 
+/* async function pintar() {
     const data = await getEarth();
     console.log(data)
-}
-
-
-
-
-/* //Dibujar los marcadores de cada terremoto en el mapa
-for (let i = 0; i < array.length; i++) {
-
-    //
-    const map2 = L.map('map2').setView([data.features[i].geometry.coordinates[1], data.features[i].geometry.coordinates[2]]);
-
-    //pinta lals baldosas (apariencia)
-    L.tileLayer.provider('OpenTopoMap').addTo(map2);
-
-    //pinta el símbolo de ubicación
-    const marker = L.marker([position.coords.latitude.toFixed(4),position.coords.longitude.toFixed(4)]).addTo(map);
 } */
 
-
-
-//Dibujar los marcadores de cada terremoto en el mapa
-
-
-
-
-
-
-
+/* L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map); */
